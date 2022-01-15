3d87f0b0c6be9a3c478b58cc53642ef3f52652fc
choijongha <80575094+choijongha@users.noreply.github.com>
2022-01-15 12:23:31 +0900

    스타트 메뉴 수정
    순서대로 메뉴가 등장하게 구현

--- New Unity Project (1)/Assets/Scripts/StartMenu.cs
+++ New Unity Project (1)/Assets/Scripts/StartMenu.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class StartMenu : MonoBehaviour
 {
-    [SerializeField] Button StartButton;
-    [SerializeField] Button QuitButton;
+    [SerializeField] GameObject titleCatch;
+    [SerializeField] GameObject titleCow;
+    [SerializeField] GameObject titleAnd;
+    [SerializeField] GameObject titleHorse;
+    [SerializeField] GameObject Buttons;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("StartTitle", 1f, 1f);
     }
 
     // Update is called once per frame
@@ -33,5 +37,23 @@ public class StartMenu : MonoBehaviour
 #else
          Application.Quit();
 #endif
+    }
+    void StartTitle()
+    {
+        if (titleHorse.activeSelf)
+        {
+            Buttons.SetActive(true);
+        }
+        if (titleCow.activeSelf)
+        {
+            titleHorse.SetActive(true);
+        }
+
+        if (titleCatch.activeSelf)
+        {
+            titleCow.SetActive(true);
+            titleAnd.SetActive(true);
+        }
+        titleCatch.SetActive(true);
     }
 }
